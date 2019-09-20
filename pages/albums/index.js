862a37bacd2a25a258669aff95bf5aadaee9ecd6
mehanig <mehanig@gmail.com>
2019-09-20 16:11:53 +0200

    azure static website friendly

--- pages/albums/index.js
+++ pages/albums/index.js
@@ -9,12 +9,12 @@ function Albums() {
       Here is a list of all available memories. Click any blue link to open
       photo gallery:
       <br />
-* <a href="/albums/450D"> 450D </a> <br/>
-* <a href="/albums/Boston_USA_Feb_2016"> Boston_USA_Feb_2016 </a> <br/>
-* <a href="/albums/Montreal_Canada_Pycon2014"> Montreal_Canada_Pycon2014 </a> <br/>
-* <a href="/albums/Seattle,_WA,_USA"> Seattle,_WA,_USA </a> <br/>
-* <a href="/albums/florence"> florence </a> <br/>
-* <a href="/albums/italy"> italy </a> <br/>
+* <a href="/albums/450D.html"> 450D</a> <br/>
+* <a href="/albums/Boston_USA_Feb_2016.html"> Boston_USA_Feb_2016</a> <br/>
+* <a href="/albums/Montreal_Canada_Pycon2014.html"> Montreal_Canada_Pycon2014</a> <br/>
+* <a href="/albums/Seattle,_WA,_USA.html"> Seattle,_WA,_USA</a> <br/>
+* <a href="/albums/florence.html"> florence</a> <br/>
+* <a href="/albums/italy.html"> italy</a> <br/>
 
     </div>
   );
